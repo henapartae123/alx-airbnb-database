535f11fc16f442a983322f00947d3d0a227369a5
henapartae123 <liyulabs@gmail.com>
2025-09-08 09:48:14 +0300

    aggregationsqueries

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -15,6 +15,7 @@ SELECT
     p.property_id,
     p.name AS property_name,
     COUNT(b.booking_id) AS total_bookings,
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS row_number_rank,
     RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
 FROM Property p
 LEFT JOIN Booking b 
