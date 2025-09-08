ff6a934b5a47f999a2b01da923295004d5316c31
henapartae123 <liyulabs@gmail.com>
2025-09-08 09:22:33 +0300

    join queries

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,44 @@
+-- retrieves all bookings and the respective users who made those bookings
+SELECT 
+    b.booking_id,
+    b.property_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    b.status,
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email
+FROM Booking b
+INNER JOIN User u
+    ON b.user_id = u.user_id;
+
+-- retrieves all properties and their reviews, including properties that have no reviews
+SELECT 
+    p.property_id,
+    p.name AS property_name,
+    p.location,
+    p.pricepernight,
+    r.review_id,
+    r.rating,
+    r.comment,
+    r.created_at
+FROM Property p
+LEFT JOIN Review r
+    ON p.property_id = r.property_id;
+
+-- retrieves all users and all bookings, even if the user has no booking or a booking is not linked to a user
+SELECT 
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    b.booking_id,
+    b.property_id,
+    b.start_date,
+    b.end_date,
+    b.status
+FROM User u
+FULL OUTER JOIN Booking b
+    ON u.user_id = b.user_id;
