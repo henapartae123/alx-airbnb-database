c3e858658245000e8932932637c9aa4fc7c6b3f5
henapartae123 <liyulabs@gmail.com>
2025-09-08 10:10:16 +0300

    optimization

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,28 +1,27 @@
+EXPLAIN ANALYZE
 SELECT 
     b.booking_id,
     b.start_date,
     b.end_date,
     b.total_price AS booking_total_price,
     b.status AS booking_status,
-    -- User details
     u.user_id,
     u.first_name,
     u.last_name,
     u.email,
     u.role AS user_role,
-    -- Property details
     p.property_id,
     p.name AS property_name,
     p.location AS property_location,
     p.pricepernight AS property_price_per_night,
-    -- Payment details
     pay.payment_id,
     pay.amount AS payment_amount,
     pay.payment_date,
     pay.payment_method
-
 FROM Booking b
 INNER JOIN User u ON b.user_id = u.user_id
 INNER JOIN Property p ON b.property_id = p.property_id
 LEFT JOIN Payment pay ON b.booking_id = pay.booking_id
+WHERE b.status = 'confirmed' 
+  AND p.location = 'New York'
 ORDER BY b.start_date DESC;
